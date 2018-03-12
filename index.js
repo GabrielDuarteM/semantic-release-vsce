9acf631bcd71643fd513da6a4557a1c5c17da7aa
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-03-12 01:11:36 -0300

    feat(prepare): Support the prepare hook from semantic-release@^15.0.0
    This aims to provide support to the latest version of semantic-release, providing a prepare hook
    that deals with bumping the package version before publishing.
    
    BREAKING CHANGE: Support semantic-release@^15.0.0

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const verifyVsce = require('./lib/verify');
 const vscePublish = require('./lib/publish');
+const prepareVsce = require('./lib/prepare');
 
 let verified;
+let prepared;
 
 async function verifyConditions (pluginConfig, {logger}) {
   await verifyVsce(logger);
@@ -13,10 +15,21 @@ async function publish (pluginConfig, {nextRelease: {version}, logger}) {
     await verifyVsce(logger);
     verified = true;
   }
+
+  if (!prepared) {
+    await prepareVsce(pluginConfig, version, logger);
+  }
+
   await vscePublish(version, pluginConfig.packageVsix, logger);
+}
+
+async function prepare (pluginConfig, { nextRelease: { version }, logger }) {
+  await prepareVsce(pluginConfig, version, logger);
+  prepared = true;
 }
 
 module.exports = {
   verifyConditions,
+  prepare,
   publish
 };
