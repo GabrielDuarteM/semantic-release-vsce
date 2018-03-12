d5676b8a7bba2d6c2e47ae894378b6d09121af0c
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-03-12 01:46:06 -0300

    fix: Throw an exception if something goes wrong on the exported functions
    If something goes wrong, throw a SemanticReleaseError to stop semantic-release from proceding the
    release steps

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const SemanticReleaseError = require('@semantic-release/error');
 const verifyVsce = require('./lib/verify');
 const vscePublish = require('./lib/publish');
 const prepareVsce = require('./lib/prepare');
@@ -6,26 +7,38 @@ let verified;
 let prepared;
 
 async function verifyConditions (pluginConfig, {logger}) {
-  await verifyVsce(logger);
+  try {
+    await verifyVsce(logger);
+  } catch (err) {
+    throw new SemanticReleaseError('There was an error verifying the release.', 'EVERVSCE');
+  }
   verified = true;
 }
 
 async function publish (pluginConfig, {nextRelease: {version}, logger}) {
-  if (!verified) {
-    await verifyVsce(logger);
-    verified = true;
-  }
+  try {
+    if (!verified) {
+      await verifyVsce(logger);
+      verified = true;
+    }
 
-  if (!prepared) {
-    await prepareVsce(pluginConfig, version, logger);
-  }
+    if (!prepared) {
+      await prepareVsce(pluginConfig, version, logger);
+    }
 
-  await vscePublish(version, pluginConfig.packageVsix, logger);
+    await vscePublish(version, pluginConfig.packageVsix, logger);
+  } catch (err) {
+    throw new SemanticReleaseError('There was an error publishing the release.', 'EPUBVSCE');
+  }
 }
 
 async function prepare (pluginConfig, { nextRelease: { version }, logger }) {
-  await prepareVsce(pluginConfig, version, logger);
-  prepared = true;
+  try {
+    await prepareVsce(pluginConfig, version, logger);
+    prepared = true;
+  } catch (err) {
+    throw new SemanticReleaseError('There was an error preparing the release.', 'EPREPVSCE');
+  }
 }
 
 module.exports = {
